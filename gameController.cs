71a174622b7cc34caa3957a888f8beb4ef06031c
MargoJane <31864917+MargoJane@users.noreply.github.com>
2017-09-18 14:42:42 -0400

    First Commit

---
+++ gameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour {
+
+	bool printedMessage;
+
+	// Use this for initialization
+	void Start () {
+		
+		printedMessage = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > 3 && printedMessage == false) {
+			print("It's been 3 seconds!");
+			printedMessage = true;
+		}
+	}
+}
